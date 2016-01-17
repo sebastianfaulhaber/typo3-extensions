6d3e1af75a78c1fada95beac3860b5988c83dc44
Sebastian Faulhaber <sebastian.faulhaber@gmx.de>
2016-01-17 17:38:45 +0100

    Gallery & profile update are now shown in the wall.

--- cwt_community/res/class.tx_cwtcommunity_lib_wall.php
+++ cwt_community/res/class.tx_cwtcommunity_lib_wall.php
@@ -21,7 +21,7 @@
  *
  *   This copyright notice MUST APPEAR in all copies of the script!
  */
-
+include_once(t3lib_extMgm::extPath('cwt_community').'res/class.tx_cwtcommunity_lib_gallery.php');
 
 /**
  * Wall related functions used in the community using EXT:smarty.
@@ -32,20 +32,152 @@
  */
 class tx_cwtcommunity_lib_wall {
 
-	
-	public static function getWallEntriesForUser($owner_uid) {
+
+	public static function getWallEntries($limit) {
 		$conf = tx_cwtcommunity_lib_common::getConfArray();
-		$res = tx_cwtcommunity_lib_common::dbQuery('SELECT * FROM tx_cwtcommunity_wall WHERE wall_owner_uid = '.intval($owner_uid).' AND wall_entry_uid IS NULL AND deleted != 1 AND hidden != 1 ORDER BY uid DESC;');
+		$mergedResult = array();
+		$sortedResult = array();
 		
 		// Generate path to user pic
+		$res = tx_cwtcommunity_lib_common::dbQuery('SELECT * FROM tx_cwtcommunity_wall WHERE wall_entry_uid IS NULL AND deleted != 1 AND hidden != 1 ORDER BY uid DESC;');
 		for ($i = 0; $i < sizeof($res); $i++) {
+			$res[$i]['type'] = 'wall';
 			$res[$i]['cruser_pic'] = tx_cwtcommunity_lib_common::getUserPicGeneric($res[$i]['cruser_id'], $conf['wall.']['user_pic.']);
 			$res[$i]['cruser'] = tx_cwtcommunity_lib_common::getUserForDisplay($res[$i]['cruser_id']);
 			$res[$i]['cruser_linkToUsersProfile'] = tx_cwtcommunity_lib_common::getLinkToUsersProfile($res[$i]['cruser_id']);
 			$res[$i]['crdate_date'] = date(tx_cwtcommunity_lib_common::getLL('CWT_DATE_FORMAT'), $res[$i]['crdate']);
 			$res[$i]['crdate_time'] = date(tx_cwtcommunity_lib_common::getLL('CWT_TIME_FORMAT'), $res[$i]['crdate']);
+			$mergedResult[$res[$i]['crdate']] = $res[$i];
 		}
-		return $res;
+		
+		
+		// GALLERY INTEGRATION: Merge with created albums
+		$allUsers = tx_cwtcommunity_lib_common::dbQuery('SELECT DISTINCT u.uid FROM fe_users AS u, tx_cwtcommunity_albums AS a WHERE u.deleted != 1 AND u.disable != 1 AND u.uid = a.cruser_id AND u.tx_cwtcommunityuser_userlist_visibility != 1 AND u.tx_cwtcommunityuser_profile_access != 1 ORDER BY uid DESC;');
+
+		for ($k = 0; $k < sizeof($allUsers); $k++) {
+			$owner_uid = $allUsers[$k]['uid'];
+			$res02 = tx_cwtcommunity_lib_gallery::getAlbums($owner_uid);
+
+			for ($j = 0; $j < sizeof($res02); $j++) {
+				$tmp = array();
+				// Copy default fields
+				$tmp['tstamp'] = $res02[$j]['tstamp'];
+				$tmp['cruser_id'] = $res02[$j]['cruser_id'];
+				$tmp['crdate'] = $res02[$j]['crdate'];
+				$tmp['type'] = 'gallery_new_album';
+				$tmp['cruser_pic'] = tx_cwtcommunity_lib_common::getUserPicGeneric($owner_uid, $conf['wall.']['user_pic.']);
+				$tmp['cruser'] = tx_cwtcommunity_lib_common::getUserForDisplay($owner_uid);
+				$tmp['cruser_linkToUsersProfile'] = tx_cwtcommunity_lib_common::getLinkToUsersProfile($owner_uid);
+				$tmp['crdate_date'] = date(tx_cwtcommunity_lib_common::getLL('CWT_DATE_FORMAT'), $res02[$j]['crdate']);
+				$tmp['crdate_time'] = date(tx_cwtcommunity_lib_common::getLL('CWT_TIME_FORMAT'), $res02[$j]['crdate']);
+	
+				// Fill custom fields
+				$tmp['gallery_new_album'] = $res02[$j];
+				$tmp['gallery_new_album']['linkToAlbum'] = tx_cwtcommunity_lib_common::getPageLink($conf['pid_gallery'], "", array(tx_cwtcommunity_lib_constants::CONST_ACTION => "show_album_detail", "album_uid" => $res02[$j]['uid'], "uid" => $owner_uid));
+				$mergedResult[$res02[$j]['crdate']] = $tmp;
+			}			
+		}
+		
+		// PROFILE UPDATE INTEGRATION: Merge with profile update
+		$allUsers = tx_cwtcommunity_lib_common::dbQuery('SELECT DISTINCT u.uid FROM fe_users AS u WHERE u.deleted != 1 AND u.disable != 1 AND u.tx_cwtcommunityuser_userlist_visibility != 1 AND u.tx_cwtcommunityuser_profile_access != 1 ORDER BY tstamp DESC LIMIT 0,'.intval($limit).';');
+
+		for ($m = 0; $m < sizeof($allUsers); $m++) {
+			$owner_uid = $allUsers[$m]['uid'];
+			$res03 = tx_cwtcommunity_lib_common::getUser($owner_uid);
+			$tmp = array();
+			// Copy default fields
+			$tmp['tstamp'] = $res03['tstamp'];
+			$tmp['cruser_id'] = $res03['cruser_id'];
+			$tmp['crdate'] = $res03['tstamp'];
+			$tmp['type'] = 'profile_update';
+			$tmp['cruser_pic'] = tx_cwtcommunity_lib_common::getUserPicGeneric($owner_uid, $conf['wall.']['user_pic.']);
+			$tmp['cruser'] = tx_cwtcommunity_lib_common::getUserForDisplay($owner_uid);
+			$tmp['cruser_linkToUsersProfile'] = tx_cwtcommunity_lib_common::getLinkToUsersProfile($owner_uid);
+			$tmp['crdate_date'] = date(tx_cwtcommunity_lib_common::getLL('CWT_DATE_FORMAT'), $res03['tstamp']);
+			$tmp['crdate_time'] = date(tx_cwtcommunity_lib_common::getLL('CWT_TIME_FORMAT'), $res03['tstamp']);
+			$mergedResult[$res03['tstamp']] = $tmp;
+		}
+		
+		// Sort array by timestamp, whereas newest values are shown first.
+		krsort($mergedResult);
+		$keys = array_keys($mergedResult);
+		for ($x = 0; $x < sizeof($mergedResult); $x++) {
+			$sortedResult[] = $mergedResult[$keys[$x]];
+			// Limit the results
+			if ($x >= 100) {
+				break;
+			}
+		}
+		return $sortedResult;
+	}
+	
+	public static function getWallEntriesForUser($owner_uid) {
+		$conf = tx_cwtcommunity_lib_common::getConfArray();
+		$mergedResult = array();
+		$sortedResult = array();
+		
+		// Generate path to user pic
+		$res = tx_cwtcommunity_lib_common::dbQuery('SELECT * FROM tx_cwtcommunity_wall WHERE wall_owner_uid = '.intval($owner_uid).' AND wall_entry_uid IS NULL AND deleted != 1 AND hidden != 1 ORDER BY uid DESC;');
+		for ($i = 0; $i < sizeof($res); $i++) {
+			$res[$i]['type'] = 'wall';
+			$res[$i]['cruser_pic'] = tx_cwtcommunity_lib_common::getUserPicGeneric($res[$i]['cruser_id'], $conf['wall.']['user_pic.']);
+			$res[$i]['cruser'] = tx_cwtcommunity_lib_common::getUserForDisplay($res[$i]['cruser_id']);
+			$res[$i]['cruser_linkToUsersProfile'] = tx_cwtcommunity_lib_common::getLinkToUsersProfile($res[$i]['cruser_id']);
+			$res[$i]['crdate_date'] = date(tx_cwtcommunity_lib_common::getLL('CWT_DATE_FORMAT'), $res[$i]['crdate']);
+			$res[$i]['crdate_time'] = date(tx_cwtcommunity_lib_common::getLL('CWT_TIME_FORMAT'), $res[$i]['crdate']);
+			$mergedResult[$res[$i]['crdate']] = $res[$i];
+		}
+		
+		
+		// GALLERY INTEGRATION: Merge with created albums
+		$res02 = tx_cwtcommunity_lib_gallery::getAlbums($owner_uid);
+		for ($j = 0; $j < sizeof($res02); $j++) {
+			$tmp = array();
+			// Copy default fields
+			$tmp['tstamp'] = $res02[$j]['tstamp'];
+			$tmp['cruser_id'] = $res02[$j]['cruser_id'];
+			$tmp['crdate'] = $res02[$j]['crdate'];
+			$tmp['type'] = 'gallery_new_album';
+			$tmp['cruser_pic'] = tx_cwtcommunity_lib_common::getUserPicGeneric($owner_uid, $conf['wall.']['user_pic.']);
+			$tmp['cruser'] = tx_cwtcommunity_lib_common::getUserForDisplay($owner_uid);
+			$tmp['cruser_linkToUsersProfile'] = tx_cwtcommunity_lib_common::getLinkToUsersProfile($owner_uid);
+			$tmp['crdate_date'] = date(tx_cwtcommunity_lib_common::getLL('CWT_DATE_FORMAT'), $res02[$j]['crdate']);
+			$tmp['crdate_time'] = date(tx_cwtcommunity_lib_common::getLL('CWT_TIME_FORMAT'), $res02[$j]['crdate']);
+
+			// Fill custom fields
+			$tmp['gallery_new_album'] = $res02[$j];
+			$tmp['gallery_new_album']['linkToAlbum'] = tx_cwtcommunity_lib_common::getPageLink($conf['pid_gallery'], "", array(tx_cwtcommunity_lib_constants::CONST_ACTION => "show_album_detail", "album_uid" => $res02[$j]['uid']));
+			$mergedResult[$res02[$j]['crdate']] = $tmp;
+		}		
+
+
+		// PROFILE UPDATE INTEGRATION: Merge with profile update
+		$res03 = tx_cwtcommunity_lib_common::getUser($owner_uid);
+		$tmp = array();
+		// Copy default fields
+		$tmp['tstamp'] = $res03['tstamp'];
+		$tmp['cruser_id'] = $res03['cruser_id'];
+		$tmp['crdate'] = $res03['tstamp'];
+		$tmp['type'] = 'profile_update';
+		$tmp['cruser_pic'] = tx_cwtcommunity_lib_common::getUserPicGeneric($owner_uid, $conf['wall.']['user_pic.']);
+		$tmp['cruser'] = tx_cwtcommunity_lib_common::getUserForDisplay($owner_uid);
+		$tmp['cruser_linkToUsersProfile'] = tx_cwtcommunity_lib_common::getLinkToUsersProfile($owner_uid);
+		$tmp['crdate_date'] = date(tx_cwtcommunity_lib_common::getLL('CWT_DATE_FORMAT'), $res03['tstamp']);
+		$tmp['crdate_time'] = date(tx_cwtcommunity_lib_common::getLL('CWT_TIME_FORMAT'), $res03['tstamp']);
+		$mergedResult[$res03['tstamp']] = $tmp;
+
+		// Sort array by timestamp, whereas newest values are shown first.
+		krsort($mergedResult);
+		$keys = array_keys($mergedResult);
+		for ($x = 0; $x < sizeof($mergedResult); $x++) {
+			$sortedResult[] = $mergedResult[$keys[$x]];
+		}
+		return $sortedResult;
+	}
+	
+	
+	function cmp($a, $b) {
+		return $a['tstamp'] - $b['tstamp'];
 	}
 	
 	public static function getWallEntry($entry_uid) {
